c7e0db1bce641fa6e3b8658a528ac0d65542b146
Isidoro Ghezzi <isidoroghezzi@salgroup.it>
2023-04-21 23:39:53 +0200

    function fizzBuzz is now private to main

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const fizzBuzz = number => [
-	['FizzBuzz', 'Fizz'],
-	['Buzz', number],
-][Number(number % 3 !== 0)][Number(number % 5 !== 0)];
-
 const main = () => {
+	const fizzBuzz = number => [
+		['FizzBuzz', 'Fizz'],
+		['Buzz', number],
+	][Number(number % 3 !== 0)][Number(number % 5 !== 0)];	
 	const result = Array.from({length: 100}).fill().map((value, i) => fizzBuzz(i + 1));
 	console.log(JSON.stringify(result));
 };
-
 main();
