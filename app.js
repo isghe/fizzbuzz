c9156276217c5f7e2a69f64b8cadd5410e06f765
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2023-04-21 18:22:10 +0200

    npx xo --fix

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-const mod = (a, b) =>{
-    return a%b;
+const mod = (a, b) => a % b;
+
+const boolToInteger = Number;
+
+const fizzBuzz = number => [
+	['FizzBuzz', 'Fizz'],
+	['Buzz', number],
+]
+	[boolToInteger(mod(number, 3) !== 0)]
+	[boolToInteger(mod(number, 5) !== 0)];
+
+const main = () => {
+	const result = [];
+	for (let i = 0; i < 100; ++i) {
+		const output = {index: i + 1, mod3: mod(i + 1, 3), mod5: mod(i + 1, 5), fizzBuzz35: fizzBuzz(i + 1)};
+		result.push(output);
+	}
+
+	console.log(result);
 };
 
-const boolToInteger = (boolValue) =>{
-    return +boolValue;
-};
-
-const fizzBuzz = (number) => {
-    return [
-        ['FizzBuzz', 'Fizz'],
-        ['Buzz', number]
-    ]
-    [boolToInteger (0 !== mod (number, 3))]
-    [boolToInteger (0 !== mod (number, 5))];
-};
-
-const main = () =>{
-    let result = [];
-    for (let i = 0; i < 100; ++i){
-        const output = {index: i+1, mod3: mod (i+1, 3), mod5: mod (i+1, 5), fizzBuzz35: fizzBuzz (i+1)};
-        result.push (output);
-    }
-    console.log (result);
-};
-
-main ();
+main();
