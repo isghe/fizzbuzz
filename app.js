bc07b2d8004ec5fb3994b9392e5b18f83fde2533
Isidoro Ghezzi <isidoroghezzi@salgroup.it>
2023-04-21 23:50:41 +0200

    true direct logic (no double negative)

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const main = () => {
 	const fizzBuzz = number => [
-		['FizzBuzz', 'Fizz'],
-		['Buzz', number],
-	][Number(number % 3 !== 0)][Number(number % 5 !== 0)];
+		[number, 'Buzz'],
+		['Fizz', 'FizzBuzz'],
+	][Number(number % 3 === 0)][Number(number % 5 === 0)];
 	const result = Array.from({length: 100}).fill().map((value, i) => fizzBuzz(i + 1));
 	console.log(JSON.stringify(result));
 };
