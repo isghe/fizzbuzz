529a9bc8a495a6f2834dde8b31bbd69b835b48a3
Isidoro Ghezzi <isidoroghezzi@salgroup.it>
2023-04-21 21:33:31 +0200

    using fill, map

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ const fizzBuzz = number => [
 ][boolToInteger(mod(number, 3) !== 0)][boolToInteger(mod(number, 5) !== 0)];
 
 const main = () => {
-	const result = [];
-	for (let i = 0; i < 100; ++i) {
-		const output = {index: i + 1, mod3: mod(i + 1, 3), mod5: mod(i + 1, 5), fizzBuzz35: fizzBuzz(i + 1)};
-		result.push(output);
-	}
+	const result = Array(100).fill ().map ((value,i) => {
+		return {index: i + 1, mod3: mod(i + 1, 3), mod5: mod(i + 1, 5), fizzBuzz35: fizzBuzz(i + 1)};
+	});
 
 	console.log(JSON.stringify(result));
+
 };
 
 main();
